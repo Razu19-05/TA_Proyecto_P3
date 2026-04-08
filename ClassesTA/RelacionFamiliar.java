95a5b417b31347c39b338f6c07c4f46a820c7c9f
yankitei19 <a20221462@pucp.edu.pe>
2026-04-07 23:39:27 -0500

    encapsulamiento de atributos RelacionFamiliar

--- ClassesTA/RelacionFamiliar.java
+++ ClassesTA/RelacionFamiliar.java
@@ -1,8 +1,9 @@
 import java.util.List;
 import java.util.ArrayList;
+
 class RelacionFamiliar {
     private int id_relacionFamiliar;
     private Apoderado apoderado;
     private List<Alumno> alumnos;
-    String tipo; // hermano,primo
+    private String tipo; // hermano,primo
 }

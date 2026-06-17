ce85e7d5e8fa45c45995f1b7be415f86df8a3cce
yankitei19 <a20221462@pucp.edu.pe>
2026-06-16 19:52:25 -0500

    EmpeadoRS terminado

--- SIMESolution/SIME_RS/src/main/java/pe/edu/pucp/sime/sime_rs/services/EmpleadosRS.java
+++ SIMESolution/SIME_RS/src/main/java/pe/edu/pucp/sime/sime_rs/services/EmpleadosRS.java
@@ -1,9 +1,8 @@
 package pe.edu.pucp.sime.sime_rs.services;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import pe.edu.pucp.SIME.BL.PersonalBLImpl;
 import pe.edu.pucp.SIME.model.DTO.ResumenPersonalDTO;
 import pe.edu.pucp.SIME.BL.impl.IPersonalBL;
@@ -13,6 +12,7 @@ import java.util.List;
 
 @Path("EmpleadosRS")
 @Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class EmpleadosRS {
 
     private IPersonalBL personalBL = new PersonalBLImpl();
@@ -33,9 +33,48 @@ public class EmpleadosRS {
     public List<Persona> listEmpleados() {
         try {
             return personalBL.listarEmpleados();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
+
+    @POST
+    @Path("crear")
+    public Response crearEmpleado(Persona persona) {
+        try {
+            persona = personalBL.registrarNuevoEmpleado(persona);
+            return Response.status(Response.Status.CREATED).entity(persona).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PUT
+    @Path("actualizar/{id}")
+    public Response actulizarEmpleado(@PathParam("id") int id,Persona personaAct) {
+        try {
+            personaAct.setIdPersona(id);
+            personaAct = personalBL.actualizarEmpleado(personaAct);
+            return Response.ok(personaAct).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+    @DELETE
+    @Path("eliminar/{id}")
+    public Response eliminarEmpleado(@PathParam("id") int id) {
+        try{
+            Persona persona = personalBL.buscarEmpleadoPorId(id);
+            personalBL.eliminarEmpleado(persona);
+            return Response.noContent().build();
+        } catch (Exception e){
+            e.printStackTrace();
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
 }
+
+

e97dd64efb775163dceb6b284cd922c0f9fd3e8d
yankitei19 <a20221462@pucp.edu.pe>
2026-06-29 16:20:23 -0500

    ficha alumno con historial

--- SIMESolution/SIME_BL/src/main/java/pe/edu/pucp/SIME/BL/impl/IMatriculaBL.java
+++ SIMESolution/SIME_BL/src/main/java/pe/edu/pucp/SIME/BL/impl/IMatriculaBL.java
@@ -1,7 +1,10 @@
 package pe.edu.pucp.SIME.BL.impl;
 
+import pe.edu.pucp.SIME.model.DTO.MatriculaAlumnoDTO;
 import pe.edu.pucp.SIME.model.DTO.SolicitudMatriculaDTO;
 import pe.edu.pucp.SIME.model.gestionMatricula.MatriculaDetalle;
+
+import java.util.List;
 
 public interface IMatriculaBL
 {
@@ -14,4 +17,6 @@ public interface IMatriculaBL
     MatriculaDetalle insertarMatriculaDetalle(MatriculaDetalle matriculaDetalle) throws Exception;
 
     MatriculaDetalle cargarMatriculaAlumno(int idAlumno) throws Exception;
+
+    List<MatriculaAlumnoDTO> listarMatriculasPorAlumno(int idAlumno) throws Exception;
 }

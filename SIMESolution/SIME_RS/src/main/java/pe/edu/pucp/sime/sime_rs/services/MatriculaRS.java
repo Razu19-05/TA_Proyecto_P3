e678586a81293b499d368df0cc326ca0fab813dd
yankitei19 <a20221462@pucp.edu.pe>
2026-06-29 00:08:55 -0500

    matricula existente RS

--- SIMESolution/SIME_RS/src/main/java/pe/edu/pucp/sime/sime_rs/services/MatriculaRS.java
+++ SIMESolution/SIME_RS/src/main/java/pe/edu/pucp/sime/sime_rs/services/MatriculaRS.java
@@ -8,8 +8,11 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import pe.edu.pucp.SIME.BL.MatriculaBLImpl;
 import pe.edu.pucp.SIME.BL.MatriculaAlumnoNuevoBL;
+import pe.edu.pucp.SIME.BL.impl.IMatriculaBL;
 import pe.edu.pucp.SIME.BL.impl.IMatriculaAlumnoNuevoBL;
+import pe.edu.pucp.SIME.model.DTO.SolicitudMatriculaDTO;
 import pe.edu.pucp.SIME.model.DTO.MatriculaAlumnoNuevoRequestDTO;
 import pe.edu.pucp.SIME.model.DTO.MatriculaAlumnoNuevoResponseDTO;
 import pe.edu.pucp.SIME.model.DTO.VacanteMatriculaDTO;
@@ -23,6 +26,7 @@ public class MatriculaRS {
 
 	private final IMatriculaAlumnoNuevoBL matriculaAlumnoNuevoBL =
 			new MatriculaAlumnoNuevoBL();
+	private final IMatriculaBL matriculaBL = new MatriculaBLImpl();
 
 	@GET
 	@Path("vacantes")
@@ -59,6 +63,21 @@ public class MatriculaRS {
 		} catch (Exception e) {
 			e.printStackTrace();
 
+			return Response.status(Response.Status.BAD_REQUEST)
+					.entity(e.getMessage())
+					.build();
+		}
+	}
+	@POST
+	@Path("alumnoExistente")
+	public Response matricularAlumnoExistente(SolicitudMatriculaDTO request) {
+		try {
+			matriculaBL.procesarMatriculaCompleta(request);
+			return Response.status(Response.Status.CREATED)
+					.entity(request)
+					.build();
+		} catch (Exception e) {
+			e.printStackTrace();
 			return Response.status(Response.Status.BAD_REQUEST)
 					.entity(e.getMessage())
 					.build();
